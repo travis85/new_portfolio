53c139cca1ea887947f84a5b5e0bc51d23cc7a72
travis flake <tflake83@gmail.com>
2022-08-01 19:55:24 -0400

    routes

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Styles from '../styles/Header.module.css'
-
+import {TbBrandJavascript} from 'react-icons/tb'
+import { TbBrandHtml5 } from 'react-icons/tb'
+import { TbBrandCss3 } from 'react-icons/tb'
+import { FaReact } from 'react-icons/fa'
+import {TbBrandTailwind} from 'react-icons/tb'
 
 export default function Header() {
   return (
@@ -10,23 +14,28 @@ export default function Header() {
     <div className={Styles.container}>
       
       <div className={Styles.links}>
-        
           <ul className="breadcrumb">
-            <Link href="https://github.com/travis85">
+            <Link href="/">
               <li className="breadcrumb-item"><a href="#">HOME</a></li>
             </Link>
-            <Link href="https://github.com/travis85">
-              <li className="breadcrumb-item"><a href="#">SKILLS</a></li>
+            <Link href="/projects">
+              <li className="breadcrumb-item"><a href="#">PROJECTS</a></li>
             </Link>
-            <Link href="https://github.com/travis85">
-              <li className="breadcrumb-item"><a href="#">FEATURED PROJECTS</a></li>
-            </Link>
+            <li className="breadcrumb-item">
+              <a href="https://drive.google.com/file/d/19FjHMmQm443AgSDAO_XW914SDG0B9-r_/view?usp=sharing">RESUME</a>
+            </li>
             
-          
-          
+              <li className="breadcrumb-item"><a href="https://drive.google.com/file/d/1196mvlZeVmM_tcPBBnstl_21K4gLSNeM/view?usp=sharing">RECOMMENDATIONS</a></li>
+            
         </ul>
-        
-      </div>
+        </div>
+        <div className={Styles. skills}>
+          <TbBrandJavascript size="25px" id={Styles.jsIcon} />
+          <TbBrandHtml5 size="25px" id={Styles.jsIcon} />
+          <TbBrandCss3 size="25px" id={Styles.jsIcon} />
+          <FaReact size="25px" id={Styles.jsIcon} />
+          <TbBrandTailwind size="25px" id={Styles.jsIcon} />
+        </div>
       
     </div>
     </>
